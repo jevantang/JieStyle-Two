fee9eff5d323fcfd926134df3bc2e11b4e1ecc51
Jarvis <Jarvis.Tang@qq.com>
2017-07-02 02:47:59 +0800

    去除页面头部无用代码

--- functions.php
+++ functions.php
@@ -1,30 +1,42 @@
 <?php
 function tangstyle_page_menu_args( $args ) {
-	$args['show_home'] = true;
-	return $args;
+  $args['show_home'] = true;
+  return $args;
 }
 add_filter( 'wp_page_menu_args', 'tangstyle_page_menu_args' );
 if ( ! function_exists( 'tangstyle_content_nav' ) ) :
 register_nav_menus(array('header-menu' => __( 'JieStyle导航菜单' ),));
 
 //去除头部无用代码
-remove_action( 'wp_head', 'feed_links', 2 );   
-remove_action( 'wp_head', 'feed_links_extra', 3 );   
-remove_action( 'wp_head', 'rsd_link' );   
-remove_action( 'wp_head', 'wlwmanifest_link' );   
-remove_action( 'wp_head', 'index_rel_link' );   
-remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );   
-remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );   
-remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );   
-remove_action( 'wp_head', 'locale_stylesheet' );   
-remove_action( 'publish_future_post', 'check_and_publish_future_post', 10, 1 );   
-remove_action( 'wp_head', 'noindex', 1 );   
-remove_action( 'wp_head', 'wp_print_head_scripts', 9 );   
-remove_action( 'wp_head', 'wp_generator' );   
-remove_action( 'wp_head', 'rel_canonical' );   
-remove_action( 'wp_footer', 'wp_print_footer_scripts' );   
-remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );   
-remove_action( 'template_redirect', 'wp_shortlink_header', 11, 0 ); 
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+remove_action( 'wp_head', 'rsd_link' );
+remove_action( 'wp_head', 'wlwmanifest_link' );
+remove_action( 'wp_head', 'index_rel_link' );
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 );
+remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0 );
+remove_action( 'wp_head', 'locale_stylesheet' );
+remove_action( 'wp_head', 'noindex', 1 );
+remove_action( 'wp_head', 'wp_print_head_scripts', 9 );
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'rel_canonical' );
+remove_action( 'wp_head', 'wp_shortlink_wp_head', 10, 0 );
+remove_action( 'wp_head', 'wp_oembed_add_host_js');
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
+remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
+remove_action( 'wp_footer', 'wp_print_footer_scripts' );
+remove_action( 'publish_future_post', 'check_and_publish_future_post', 10, 1 );
+remove_action( 'template_redirect', 'wp_shortlink_header', 11, 0 );
+remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+remove_action( 'rest_api_init', 'wp_oembed_register_route');
+remove_filter( 'rest_pre_serve_request', '_oembed_rest_pre_serve_request', 10, 4);
+remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10);
+remove_filter( 'oembed_response_data', 'get_oembed_response_data_rich', 10, 4);
+
+add_filter('rest_enabled', '__return_false');
+add_filter('rest_jsonp_enabled', '__return_false');
 
 //启动友情链接
 add_filter( 'pre_option_link_manager_enabled', '__return_true' );
@@ -35,6 +47,14 @@ $avatar = preg_replace( "/http:\/\/(www|\d).gravatar.com/","https://cn.gravatar.
 return $avatar;
 }
 add_filter( 'get_avatar', 'kratos_get_avatar' );
+
+//移除菜单的多余CSS选择器
+add_filter('nav_menu_css_class', 'my_css_attributes_filter', 100, 1);
+add_filter('nav_menu_item_id', 'my_css_attributes_filter', 100, 1);
+add_filter('page_css_class', 'my_css_attributes_filter', 100, 1);
+function my_css_attributes_filter($var) {
+  return is_array($var) ? array() : '';
+}
 
 //评论模板
 function tangstyle_comment( $comment, $args, $depth ) {
@@ -72,6 +92,26 @@ function tangstyle_comment( $comment, $args, $depth ) {
   endswitch;
 }
 endif;
+
+//禁用Emoji表情
+function disable_emojis() {
+ remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+ remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+ remove_action( 'wp_print_styles', 'print_emoji_styles' );
+ remove_action( 'admin_print_styles', 'print_emoji_styles' ); 
+ remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+ remove_filter( 'comment_text_rss', 'wp_staticize_emoji' ); 
+ remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+ add_filter( 'tiny_mce_plugins', 'disable_emojis_tinymce' );
+}
+add_action( 'init', 'disable_emojis' );
+function disable_emojis_tinymce( $plugins ) {
+ if ( is_array( $plugins ) ) {
+ return array_diff( $plugins, array( 'wpemoji' ) );
+ } else {
+ return array();
+ }
+}
 
 // 获得热评文章
 function tangstyle_get_most_viewed($posts_num=10, $days=180){
