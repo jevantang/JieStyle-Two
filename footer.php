e8b241510727fc11794d1168e627c88f0ef23f25
tecyang <me@tecyang.com>
2018-04-19 20:17:58 +0800

    时间轴文章archive页面

--- footer.php
+++ footer.php
@@ -35,6 +35,12 @@ $(function() {
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(bp, s);
 })();
+$('.archives ul.archives-monthlisting').hide();
+$('.archives ul.archives-monthlisting:first').show();
+$('.archives .m-title').click(function() {
+    $(this).next().slideToggle('fast');
+    return false;
+});
 </script>
 </body>
 </html>
